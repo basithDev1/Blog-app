54e2c0500aa2609eab6f4ea5d178ce6bcfe96f0b
basith <basithunny@gmail.com>
2024-07-25 02:01:14 +0530

    completed the headder component , mainly used flow-bite react learned lot of things and the structure how react and tailwind work together

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import About from "./Pages/About";
 import Home from "./Pages/home.jsx";
+import Headder from "./components/Headder.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Headder/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ client/src/components/Headder.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Navbar, TextInput, Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import { FaMoon } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
+
+export default function Headder() {
+  const path = useLocation().pathname;
+  return (
+    <Navbar>
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-blue-500 to-green-500 rounded-lg text-white">
+          basith
+        </span>
+        blog
+      </Link>
+      <form action="">
+        <TextInput
+          type="text"
+          placeholder="Search"
+          rightIcon={CiSearch}
+          className="hidden lg:inline"
+        ></TextInput>
+      </form>
+      <Button className="w-12 h-10  lg:hidden" color="gray" pill>
+        <CiSearch />
+      </Button>
+      <div className="flex gap-2">
+        <Button className="w-12 h-10 hidden lg:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue" className="h-10 w-15" pill>
+            Sign In
+          </Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
